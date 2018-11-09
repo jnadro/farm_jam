13a4131fc28e02eb984bfd237922eb735eb26b59
jnadro <jason.nadro@gmail.com>
2018-11-08 22:42:08 -0600

    Add progress bar
    - Adding directory structure for scripts, scenes, and resources

---
+++ scripts/ProgressBar.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+signal progress_completed
+
+# class member variables go here, for example:
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func start(duration):
+	$Tween.interpolate_property($TextureProgress,
+            'value', 0, 100,
+            duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Tween_tween_completed(object, key):
+	emit_signal("progress_completed")
