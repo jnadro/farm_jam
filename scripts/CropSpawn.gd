5d4e610e96579a4d65e4c5096a8fa293d9d107d0
jnadro <jason.nadro@gmail.com>
2018-11-12 21:58:09 -0600

    Add states to the crop spawn
    - Planting, Growing and Harvest states dictates how you can interact with the crop spawn
    - Currently harvesting transitions the spawn back into plantable.

--- scripts/CropSpawn.gd
+++ scripts/CropSpawn.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 # class member variables go here, for example:
+enum STATES { PLANT, GROWING, HARVEST }
 onready var seed_pouch = get_node("/root/Game/SeedPouch")
 onready var ProgressBar = preload("res://scenes/ProgressBar.tscn")
 onready var Crop = preload("res://scenes/Crop.tscn")
+var state = STATES.PLANT
 var pressed = false
 var progress_bar = null
 var planting_time = 1.0
@@ -22,38 +24,59 @@ func occupied():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 
+func start_progress(callback):
+	progress_bar = ProgressBar.instance()
+	add_child(progress_bar)
+	progress_bar.start(planting_time)
+	progress_bar.connect("progress_completed", self, callback)
+	
 func stop_progress():
 	remove_child(progress_bar)
 	progress_bar = null
 
 func _on_CropSpawn_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
-		if !occupied() && player_nearby && seed_pouch.has_seeds():
-			if event.pressed:
-				pressed = true
-				progress_bar = ProgressBar.instance()
-				add_child(progress_bar)
-				progress_bar.start(planting_time)
-				progress_bar.connect("progress_completed", self, "_planting_completed")
-			elif !event.pressed and pressed:
-				stop_progress()
+		if state == STATES.PLANT:
+			if !occupied() && player_nearby && seed_pouch.has_seeds():
+				if event.pressed:
+					pressed = true
+					start_progress("_planting_completed")
+				elif !event.pressed and pressed:
+					stop_progress()
+		elif state == STATES.HARVEST:
+			if player_nearby:
+				if event.pressed:
+					pressed = true
+					start_progress("_harvest_completed")
+				elif !event.pressed and pressed:
+					stop_progress()
 		
 func _planting_completed():
 	stop_progress()
 	
 	var crop_seed = seed_pouch.get_seed() 
 	crop = Crop.instance()
+	crop.connect("harvestable", self, "_on_crop_harvestable")
 	add_child(crop)
+	$Highlight.hide()
+	state = STATES.GROWING
+	
+func _on_crop_harvestable():
+	print("Harvest ready!")	
+	state = STATES.HARVEST
 
+func _harvest_completed():
+	print("Harvest completed")
+	stop_progress()
+	state = STATES.PLANT
 
 func _on_CropSpawn_mouse_exited():
 	if progress_bar:
 		stop_progress()
 
-
 func _on_CropSpawn_area_entered(area):
 	if area.is_in_group("Player"):
-		if !occupied():
+		if state == STATES.PLANT or state == STATES.HARVEST:
 			$Highlight.show()		
 		player_nearby = true
 
