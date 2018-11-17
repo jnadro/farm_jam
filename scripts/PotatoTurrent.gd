b48c37243cdc6e1a1618ca16b0821d523eb287e7
jnadro <jason.nadro@gmail.com>
2018-11-16 22:52:04 -0600

    Ability to interact with turret to add ammo
    - Stub out bullet class
    - Add potato turret to scene

--- scripts/PotatoTurrent.gd
+++ scripts/PotatoTurrent.gd
@@ -1,19 +1,46 @@
 extends Area2D
 
 # class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var ammo = 0
+var active = false
+var player_nearby = false
+var ammo_reload_time = 1.0
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
+	
+func start_progress():
+	$ProgressBar.visible = true
+	$ProgressBar.start(ammo_reload_time)
+	# progress_bar.connect("progress_completed", self, callback)
+	
+func stop_progress():
+	$ProgressBar.visible = false
+	$ProgressBar.stop()	
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
+func _process(delta):
+	if player_nearby:
+		if !active && Input.is_action_pressed("interact"):
+			start_progress()
+			active = true
+		elif active && Input.is_action_just_released("interact"):
+			stop_progress()
+			active = false
 
 func _on_PotatoTurrent_area_entered(area):
-	pass # replace with function body
+	if area.is_in_group("Player"):
+		player_nearby = true
+
+func _on_PotatoTurrent_area_exited(area):
+	if area.is_in_group("Player"):
+		player_nearby = false
+		active = false
+		stop_progress()
+
+func _on_FireCooldown_timeout():
+	pass
+
+
+
