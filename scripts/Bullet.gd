210dfcd0e7630f8524111ab2c2951c132815d262
jnadro <jason.nadro@gmail.com>
2018-11-19 14:51:57 -0600

    Tweak bullet collision size
    - This makes it look like it actually comes in contact with the mob, but probably will cause the turret to miss more.
    - Remove the hitbox collision because it doesn't work and isn't needed.

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -13,9 +13,10 @@ func _ready():
 	
 func _physics_process( delta ):
 	var collision = move_and_collide( vel * dir * delta )
-	# need to check for collision with a mob
 	if collision != null:
-		queue_free()
+		var collider = collision.get_collider()
+		if collider is mob:
+			queue_free()
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -25,8 +26,3 @@ func _physics_process( delta ):
 # when we leave the screen
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
-
-# when we hit something
-func _on_Hitbox_area_entered(area):
-	if (area is mob):	# this doesn't work
-		queue_free()
