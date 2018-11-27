446ab58d0411312a16430cb40d603e0afddd2e45
jnadro <jason.nadro@gmail.com>
2018-11-27 06:39:01 -0600

    Pack has generic spawn locations
    - Randomly pick a mob type to spawn at each location

--- scripts/MobSpawner.gd
+++ scripts/MobSpawner.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-export (PackedScene) var Mob
 export (float) var SpawnRate
+export (PackedScene) var Cow
+export (PackedScene) var Chicken
+export (PackedScene) var Pig
+export (PackedScene) var Pack
 
 func _ready():
 	$MobTimer.wait_time = SpawnRate
@@ -13,12 +16,27 @@ func get_random_position():
 	return $MobPath/MobSpawnLocation.position
 
 func _on_MobTimer_timeout():
-	# spawn a mob
-	var mob = Mob.instance()
-	mob.position = get_random_position()
-	mob.connect("die", self, "_on_mob_die")
-	mob.add_to_group("mobs")
-	get_parent().add_child(mob)
+	var num_spawns = 3
+	var pack = Pack.instance()
+	pack.position = get_random_position()
+	
+	for i in range(num_spawns):
+		var rand_int = 1#randi() % 3
+		var mob_type = Chicken
+		match rand_int:
+			0:
+				mob_type = Cow
+			1:
+				mob_type = Chicken
+			2:
+				mob_type = Pig
+		# spawn a mob
+		var mob = mob_type.instance()
+		mob.connect("die", self, "_on_mob_die")
+		mob.add_to_group("mobs")
+		pack.get_node("Spawn" + str(i)).add_child(mob)
+		
+	get_parent().add_child(pack)
 	
 	# start the time again
 	$MobTimer.start()
@@ -33,5 +51,6 @@ func _on_mob_die(mob):
 	var rand_y = randf() * 2.0 - 1.0
 	var spawn_offset = Vector2(rand_x * seed_spawn_radius, rand_y * seed_spawn_radius)
 	GameState.spawn_random_seed(mob.global_position + spawn_offset)
-	get_parent().remove_child(mob)
+	mob.get_parent().remove_child(mob)
+	mob.queue_free()
 	
