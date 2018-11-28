bb5ed9c1dc8311cfc3984c74731a639abaaf5182
jnadro <jason.nadro@gmail.com>
2018-11-27 20:06:57 -0600

    Connect the start button on the game over screen

--- scripts/LevelLoader.gd
+++ scripts/LevelLoader.gd
@@ -13,15 +13,23 @@ func _ready():
 	main_menu.connect("start_game", self, "_on_start_game")
 	add_child(main_menu)
 	
+func clean():
+	if main_menu:
+		remove_child(main_menu)
+		main_menu = null
+	if game_over:
+		remove_child(game_over)
+		game_over = null
+	
 func _on_start_game():
 	game = GameScene.instance()
 	game.connect("game_over", self, "_on_game_over")
-	remove_child(main_menu)
-	main_menu = null
+	clean()
 	add_child(game)
 	
 func _on_game_over():
 	game_over = GameOverScene.instance()
+	game_over.connect("start_game", self, "_on_start_game")
 	remove_child(game)
 	game = null
 	add_child(game_over)
