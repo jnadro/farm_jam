21cb82b281277ce0699a121af0deb3518e6e536c
jnadro <jason.nadro@gmail.com>
2018-11-29 23:04:43 -0600

    Spawn increasing amount of mobs (tops out at 5)
    Wave 0, 1, 2: 1 mob
    Wave 3, 4, 5: 2 mob
    Wave 6, 7, 8: 3 mob
    Wave 9,10,11: 4 Mob
    Wave 12+: 5

--- scripts/MobSpawner.gd
+++ scripts/MobSpawner.gd
@@ -5,6 +5,7 @@ var Cow
 var Bluejay
 var Pig
 export (float) var SpawnRate
+var wave_idx = 0
 
 func _ready():
 	$MobTimer.wait_time = SpawnRate
@@ -14,23 +15,42 @@ func _ready():
 	Bluejay = load("res://scenes/Bluejay.tscn")
 	Pig = load("res://scenes/Pig.tscn")
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
 
 func get_random_position():	
 	# Choose a random location on Path2D.
 	$MobPath/MobSpawnLocation.set_offset(randi())
 	return $MobPath/MobSpawnLocation.position
+	
+func get_mob_spawn_count():
+	if wave_idx < 3:
+		return 1
+	elif wave_idx < 6:
+		return 2
+	elif wave_idx < 9:
+		return 3
+	elif wave_idx < 12:
+		return 4
+	return 5
+	
+func get_random_point_in_circle(center):
+	var r = 64.0
+	var rand_x = randf() * 2.0 - 1.0
+	var rand_y = randf() * 2.0 - 1.0
+	var offset = Vector2(rand_x * r, rand_y * r)
+	return center + offset
 
 func _on_MobTimer_timeout():
-	# spawn a mob
-	var mob = random_instance()
-	mob.position = get_random_position()
-	mob.connect("die", self, "_on_mob_die")
-	mob.add_to_group("mobs")
-	get_parent().add_child(mob)
+	var count = get_mob_spawn_count()
+	var wave_position = get_random_position()
+	for i in range(count):
+		# spawn a mob
+		var mob = random_instance()
+		mob.position = get_random_point_in_circle(wave_position)
+		mob.connect("die", self, "_on_mob_die")
+		mob.add_to_group("mobs")
+		get_parent().add_child(mob)
+	
+	wave_idx += 1
 	
 	# start the time again
 	$MobTimer.start()
